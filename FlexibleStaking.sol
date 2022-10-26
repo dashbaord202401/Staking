2416474f8c0009f97141d00f98f991309d98a92d
Anto <72078695+0xAnto@users.noreply.github.com>
2022-10-26 23:51:15 +0530

    Update FlexibleStaking.sol

--- FlexibleStaking.sol
+++ FlexibleStaking.sol
@@ -3,47 +3,46 @@ pragma solidity ^0.8.15;
 
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/interfaces/IERC20.sol";
+import "./BetToken.sol";
 
 contract FlexibleStaking is ReentrancyGuard, Ownable {
-
     uint256 public MIN_STAKE_AMOUNT = 0;
-    bool public CONTRACT_RENOUNCED = false;
+    bool private CONTRACT_RENOUNCED = false;
 
-    string private constant NEVER_CONTRIBUTED_ERROR = "This address has never contributed to the protocol";
-    string private constant NO_ETH_CONTRIBUTIONS_ERROR = "No Contributions";
-    string private constant MINIMUM_CONTRIBUTION_ERROR = "Contributions must be over the minimum contribution amount";
-    
+    string private constant ZERO_BAL = "Zero Balance";
+    string private constant NO_CONTRIBUTION = "No Contributions";
+    string private constant NEVER_CONTRIBUTED = "Address has never contributed";
+    string private constant MIN_CONTRIBUTION =
+        "Amount less than minimum contribution amount";
 
     struct Staker {
-      address addr; 
-      uint256 lifetime_contribution;
-      uint256 contribution;
-      uint256 yield;
-      uint256 unstakeable;
-      uint256 joined;
-      bool exists;
+        address addr;
+        uint256 previous_contribution;
+        uint256 contribution;
+        uint256 joined;
+        bool exists;
     }
 
     mapping(address => Staker) public stakers;
     address[] public stakerList;
+    address[] private auxArray;
+    BetToken betToken;
 
-    constructor(uint256 _min_stake_amount) ReentrancyGuard() payable {
-      MIN_STAKE_AMOUNT = _min_stake_amount;
+    constructor(uint256 _min_stake_amount, BetToken _pmknToken)
+        payable
+        ReentrancyGuard()
+    {
+        betToken = _pmknToken;
+        MIN_STAKE_AMOUNT = _min_stake_amount;
     }
 
     receive() external payable {}
+
     fallback() external payable {}
 
-    function AddStakerYield(address addr, uint256 a) private {
-      stakers[addr].yield = stakers[addr].yield + a;
-    }
-
-    function RemoveStakerYield(address addr, uint256 a) private {
-      stakers[addr].yield = stakers[addr].yield - a;
-    }
-
     function RenounceContract() external onlyOwner {
-      CONTRACT_RENOUNCED = true;
+        CONTRACT_RENOUNCED = true;
     }
 
     function ChangeMinimumStakingAmount(uint256 a) external onlyOwner {
@@ -51,121 +50,142 @@ contract FlexibleStaking is ReentrancyGuard, Ownable {
     }
 
     function UnstakeAll() external onlyOwner {
-        if(CONTRACT_RENOUNCED == true){revert("Contract Renounced");}
-        for (uint i = 0; i < stakerList.length; i++) {
+        if (CONTRACT_RENOUNCED == true) {
+            revert("Contract Renounced");
+        }
+        for (uint256 i = 0; i < stakerList.length; i++) {
             address user = stakerList[i];
-            if(stakers[user].unstakeable>0){   
-            RemoveStake(user);
+            if (stakers[user].contribution > 0) {
+                RemoveStake(user);
             }
+        }
+        delete stakerList;
+    }
+
+    function Stake(uint256 amount) external payable nonReentrant {
+        require(msg.value >= MIN_STAKE_AMOUNT, MIN_CONTRIBUTION);
+        address user = msg.sender;
+        betToken.transferFrom(msg.sender, address(this), amount);
+
+        if (isStakerExists(user)) {
+            uint256 yieldTotal = calculateYieldTotal(user);
+            uint256 totalReward = stakers[msg.sender].contribution + yieldTotal;
+            stakers[msg.sender].previous_contribution = totalReward;
+            stakers[user].joined = block.timestamp;
+            stakers[msg.sender].contribution = totalReward + amount;
+        } else {
+            Staker memory newUser;
+            newUser.addr = user;
+            newUser.contribution = amount;
+            newUser.previous_contribution = amount;
+            newUser.exists = true;
+            newUser.joined = block.timestamp;
+            stakers[user] = newUser;
+            stakerList.push(user);
         }
     }
 
-    function Stake() external nonReentrant payable {
-      require(msg.value >= MIN_STAKE_AMOUNT, MINIMUM_CONTRIBUTION_ERROR);
-      uint256 unstakeable = msg.value;
-
-      if(StakerExists(msg.sender)){
-        stakers[msg.sender].lifetime_contribution = stakers[msg.sender].lifetime_contribution + unstakeable;
-        stakers[msg.sender].contribution = stakers[msg.sender].contribution + unstakeable;
-        stakers[msg.sender].unstakeable = stakers[msg.sender].unstakeable + unstakeable;
-      }else{
-        Staker memory user;
-        user.addr = msg.sender;
-        user.contribution = unstakeable;
-        user.lifetime_contribution = unstakeable;
-        user.yield = 0;
-        user.exists = true;
-        user.unstakeable = unstakeable;
-        user.joined = block.timestamp;
-        stakers[msg.sender] = user;
-        stakerList.push(msg.sender);
-      }
-    }
-
     function Unstake() external {
-      address user = msg.sender;
-      if(!StakerExists(user)){ revert(NEVER_CONTRIBUTED_ERROR); }
-      uint256 uns = stakers[user].unstakeable;
-      require(uns>0, "Zero balance");
-      uint256 yieldTotal = calculateYieldTotal(user);
-      uint256 totalReward = uns + yieldTotal;
-      stakers[user].unstakeable = 0;
-      stakers[user].contribution = 0;
-      stakers[user].joined = 0;
-      payable(user).transfer(totalReward);
+        address user = msg.sender;
+        if (!isStakerExists(user)) {
+            revert(NEVER_CONTRIBUTED);
+        }
+        uint256 uns = stakers[user].contribution +
+            stakers[user].previous_contribution;
+        require(uns > 0, "Zero balance");
+        uint256 yieldTotal = calculateYieldTotal(user);
+        uint256 totalReward = uns + yieldTotal;
+        stakers[user].contribution = 0;
+        stakers[user].joined = 0;
+        stakerList = remove(user, stakerList);
+        delete auxArray;
+        betToken.transfer(user, totalReward);
     }
 
     function RemoveStake(address user) private {
-      if(!StakerExists(user)){ revert(NEVER_CONTRIBUTED_ERROR); }
-      uint256 uns = stakers[user].unstakeable;
-      require(uns>0, "Zero balance");
-       uint256 yieldTotal = calculateYieldTotal(user);
-      uint256 totalReward = uns + yieldTotal;
-      stakers[user].unstakeable = 0;
-      stakers[user].contribution = 0;
-      stakers[user].joined = 0;
-      payable(user).transfer(totalReward);
-
+        if (!isStakerExists(user)) {
+            revert(NEVER_CONTRIBUTED);
+        }
+        uint256 uns = stakers[user].contribution +
+            stakers[user].previous_contribution;
+        require(uns > 0, "Zero balance");
+        uint256 yieldTotal = calculateYieldTotal(user);
+        uint256 totalReward = uns + yieldTotal;
+        stakers[user].contribution = 0;
+        stakers[user].joined = 0;
+        stakerList = remove(user, stakerList);
+        delete auxArray;
+        betToken.transfer(user, totalReward);
     }
-    
-    function calculateYieldTime(address user) public view returns(uint256){
+
+    function calculateYieldTime(address user) public view returns (uint256) {
         uint256 end = block.timestamp;
         uint256 totalTime = end - stakers[user].joined;
         return totalTime;
     }
 
-    function calculateYieldTotal(address user) public view returns(uint256) {
+    function calculateYieldTotal(address user) public view returns (uint256) {
         uint256 time = calculateYieldTime(user);
-        uint256 rawYield = stakers[user].unstakeable * time / 3.154e7; 
+        uint256 rawYield = (stakers[user].contribution * time) / 3.154e7;
         return rawYield;
     }
 
-    function StakerExists(address a) public view returns(bool){
-      return stakers[a].exists;
+    function remove(address user, address[] storage _array)
+        private
+        returns (address[] memory)
+    {
+        for (uint256 i = 0; i < _array.length; i++) {
+            if (_array[i] != user) auxArray.push(_array[i]);
+        }
+
+        return auxArray;
     }
 
-    function StakerCount() public view returns(uint256){
-      return stakerList.length;
+    function isStakerExists(address a) public view returns (bool) {
+        return stakers[a].exists;
     }
 
-    function GetStakeJoinDate(address a) public view returns(uint256){
-      if(!StakerExists(a)){revert(NEVER_CONTRIBUTED_ERROR);}
-      return stakers[a].joined;
+    function StakerCount() public view returns (uint256) {
+        return stakerList.length;
     }
 
-    function GetStakerYield(address a) public view returns(uint256){
-      if(!StakerExists(a)){revert(NEVER_CONTRIBUTED_ERROR);}
-      return stakers[a].yield;
-    }
-  
-    function GetStakingAmount(address a) public view returns (uint256){
-      if(!StakerExists(a)){revert(NEVER_CONTRIBUTED_ERROR);}
-      return stakers[a].contribution;
+    function GetStakingAmount(address a) public view returns (uint256) {
+        if (!isStakerExists(a)) {
+            revert(NEVER_CONTRIBUTED);
+        }
+        return stakers[a].contribution;
     }
 
-    function GetStakerPercentageByAddress(address a) public view returns(uint256){
-      if(!StakerExists(a)){revert(NEVER_CONTRIBUTED_ERROR);}
-      uint256 c_total = 0;
-      for (uint i = 0; i < stakerList.length; i++) {
-         c_total = c_total + stakers[stakerList[i]].contribution;
-      }
-      if(c_total == 0){revert(NO_ETH_CONTRIBUTIONS_ERROR);}
-      return (stakers[a].contribution * 10000) / c_total;
+    function GetStakerPercentageByAddress(address a)
+        public
+        view
+        returns (uint256)
+    {
+        if (!isStakerExists(a)) {
+            revert(NEVER_CONTRIBUTED);
+        }
+        uint256 c_total = 0;
+        for (uint256 i = 0; i < stakerList.length; i++) {
+            c_total = c_total + stakers[stakerList[i]].contribution;
+        }
+        if (c_total == 0) {
+            revert(NO_CONTRIBUTION);
+        }
+        return (stakers[a].contribution * 10000) / c_total;
     }
 
-    function GetStakerUnstakeableAmount(address addr) public view returns(uint256) {
-      if(StakerExists(addr)){ return stakers[addr].unstakeable; }else{ return 0; }
+    function GetLifetimeContributionAmount(address a)
+        public
+        view
+        returns (uint256)
+    {
+        if (!isStakerExists(a)) {
+            revert("This address has never contributed DAI to the protocol");
+        }
+        return stakers[a].previous_contribution;
     }
 
-    function GetLifetimeContributionAmount(address a) public view returns (uint256){
-      if(!StakerExists(a)){revert("This address has never contributed DAI to the protocol");}
-      return stakers[a].lifetime_contribution;
+    function CheckContractRenounced() external view returns (bool) {
+        return CONTRACT_RENOUNCED;
     }
-
-    function CheckContractRenounced() external view returns(bool){
-      return CONTRACT_RENOUNCED;
-    }
-
-
-
 }
